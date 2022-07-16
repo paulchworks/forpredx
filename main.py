ecac08e9b931eb42226bb949212984f1ab599d0c
PaulchWorks <107820572+paulchworks@users.noreply.github.com>
2022-07-16 08:13:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-install yfinance --upgrade --no-cache-dir
+!pip install yfinance --upgrade --no-cache-dir
 
 import json
 import pandas as pd
@@ -6,7 +6,7 @@ import numpy as np
 import types
 import streamlit as st
 from datetime import date
-import yfinance as yf
+import fix_yahoo_finance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
