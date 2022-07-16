4844ef4b17d1564d247b6a6f7b94f3bcf40eb9de
PaulchWorks <107820572+paulchworks@users.noreply.github.com>
2022-07-16 09:04:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-pip install -U --force-reinstall --yfinance
-
 import json
 import pandas as pd
 import numpy as np
 import types
 import streamlit as st
 from datetime import date
-import yfinance as yf
+import Yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
